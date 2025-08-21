c92fd7fafaa7e68c66a77f6d6712631aaf616530
YMonk2022 <bohnatbo@gmail.com>
2025-08-21 20:30:32 +0300

    Updated 'config,py'

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ import os
 
 SECRET_KEY = os.getenv("SECRET_KEY") or secrets.token_urlsafe(64)
 
-DEBUG = os.getenv("DEBUG", False).lower() in ("true", "1", "yes")
+DEBUG = os.getenv('DEBUG', False)
 
 PORT = int(os.getenv("PORT", 5000))
 
@@ -12,4 +12,4 @@ JWT_ACCESS_TOKEN_EXPIRES = timedelta(
     seconds=int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES", 3600))
 )
 
-WTF_CSRF_ENABLED = os.getenv("WTF_CSRF_ENABLED", True).lower() in ("true", "1", "yes")
+WTF_CSRF_ENABLED = os.getenv("WTF_CSRF_ENABLED", True)
