35273f7c62ec1719e5892135a7b82f3c6116d89f
YMonk2022 <bohnatbo@gmail.com>
2025-08-22 13:15:48 +0300

    Updated 'config.py' & 'requirements.txt'

--- app/config.py
+++ app/config.py
@@ -1,4 +1,3 @@
-from datetime import timedelta
 import secrets
 import os
 
@@ -6,8 +5,4 @@ SECRET_KEY = os.getenv("SECRET_KEY") or secrets.token_urlsafe(64)
 
 DEBUG = os.getenv('DEBUG', False)
 
-PORT = int(os.getenv("PORT", 5000))
-
-JWT_ACCESS_TOKEN_EXPIRES = os.getenv('JWT_ACCESS_TOKEN_EXPIRES', timedelta(hours=1))
-
-WTF_CSRF_ENABLED = os.getenv("WTF_CSRF_ENABLED", True)
+PORT = os.getenv("PORT", 5000)
