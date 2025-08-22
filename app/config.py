413158ecddbb846fc9308a8c8f629133f20d44f2
YMonk2022 <bohnatbo@gmail.com>
2025-08-22 13:47:29 +0300

    Updated 'config.py'

--- app/config.py
+++ app/config.py
@@ -3,6 +3,6 @@ import os
 
 SECRET_KEY = os.getenv("SECRET_KEY") or secrets.token_urlsafe(64)
 
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "yes")
 
-PORT = os.getenv("PORT", 5000)
+PORT = int(os.getenv("PORT", "5000"))
